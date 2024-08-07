e76b576313f9ed3ddce995cb189e46c8b4b96ca3
Jonathan Medeiros <jonathan.info@gmail.com>
2024-08-07 19:28:24 -0300

    ajusts project to add for portifolio

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'upflux'`, () => {
+  it(`should have as title 'test_api'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('upflux');
+    expect(app.title).toEqual('test_api');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('upflux app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('test_api app is running!');
   });
 });
